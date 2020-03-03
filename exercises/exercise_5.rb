813245087264af6ad76157e3676a29aacb504ea6
Max Rosenthal <mxrosenthal@yahoo.com>
2020-03-02 16:57:09 -0800

    finished exercise 5. able to print off how many stores make more than a certain amount.

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,10 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_sum = Store.sum(:annual_revenue)
+@num_stores = Store.all.count
+puts "Total revenue for all stores is #{@total_sum}"
+puts "The average revenue for a store is #{@total_sum/@num_stores}"
+
+@stores_over_million = Store.where("annual_revenue > ?", 1000000).count
+puts "#{@stores_over_million} stores make more than a million annually."
