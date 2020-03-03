726b31bceca5b7286016bbc7ef222d854fd30f5d
Max Rosenthal <mxrosenthal@yahoo.com>
2020-03-02 16:47:23 -0800

    Finished exercise 4. I chained my where statements together

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,14 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+s = Store.create(name: 'Surry', annual_revenue: 224000, mens_apparel: false, womens_apparel: true);
+s = Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false);
+s = Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true);
+
+
+@mens_stores = Store.where(mens_apparel: true)
+# puts @mens_stores.length
+@mens_stores.each { |n| puts "#{n.name} makes #{n.annual_revenue} anually."}
+@womens_cheap = Store.where(womens_apparel: true).where("annual_revenue < ?", 1000000)
+# puts @womens_cheap.length
+@womens_cheap.each { |n| puts "#{n.name} carries women's clothes but only makes #{n.annual_revenue} anually."}
